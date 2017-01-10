7984c963867cc9d4d5e23fd9f5733b6f990f892c
Praveen Gunasekar <praveen.gunasekar@happiestminds.com>
2017-01-10 03:34:15 -0800

    Modified the files

--- collectRemoteServerInfo.py
+++ collectRemoteServerInfo.py
@@ -1,6 +1,6 @@
 # Purpose: Python scripts to fetch the remote server information (Domain name, Mac address of the nic cards, Number of disks)
 
-import os,sys,argparse
+import os,sys,argparse,pdb
 macList = ''
 # Function for passing the correct option and get the option values
 def getArgs():
@@ -14,28 +14,25 @@ def getArgs():
 def displayServerInfo(hostname):
    global macList
    # Fetching the mac address for the nic card attached
-   diskCmd = 'a4 ssh -q '+hostname+' "sudo ifconfig" | awk \'{print $1}\' | awk \'/.*:/ { print }\' | sed s/://g| sed /^$/d'
-   nicCardAttached=os.popen(diskCmd).read().strip()
-   splitNicInfo = nicCardAttached.split('\n')
+   diskCmd = 'a4 ssh -q '+hostname+' cat /proc/net/bonding/bond0 | grep -A 7 \"^Slave Interface\"| grep  \"^Slave\ Inter\|MII\|Permanent\"| awk \'{print $NF}\'| sed \'N;N;s/\\n/ /g\''
+   nicCardAttached=os.popen(diskCmd).read()
+   splitNicInfo = nicCardAttached.strip('\n').split('\n')
    print "Mac Address for the NIC cards of server: "+hostname
    print "---------------------------------------"
-   for ethernetAdapter in splitNicInfo:
-      if ethernetAdapter != 'lo':
-         fetchMac = 'a4 ssh -q '+hostname+' "sudo ifconfig '+ethernetAdapter+'"| grep ether| awk \'{print $2}\''
-         macadd = os.popen(fetchMac).read()
-         finalOut=ethernetAdapter+' = '+macadd
-         macList+=finalOut
-   print macList
+   for nicInfo in splitNicInfo:
+      nicInfo = nicInfo.split(' ')
+      if nicInfo[1] == 'up':
+         print nicInfo[2]
 
    # Collecting the domain name information
    domainName = 'a4 ssh -q '+hostname+' "sudo dnsdomainname"'
    domainZone=os.popen(domainName).read().strip()
-   print "DomainZone\n-------------\n"+domainZone+"\n"
+   print "\nDomainZone\n-------------\n"+domainZone+"\n"
 
    # Collecting the disk information
-   noOfDisks = 'a4 ssh -q atest301 "sudo parted -l | grep \'^Disk /dev\'" | awk \'{print $2}\' | sed s/://g'
+   noOfDisks = 'a4 ssh -q atest301 "sudo parted -l | grep \'^Disk /dev/sd\'" | awk \'{print $2}\' | sed s/://g| wc -l'
    diskAvailable=os.popen(noOfDisks).read().strip()
-   print 'DiskInfo\n------------\n',diskAvailable
+   print 'Total physical disk attached \n-----------------\n',diskAvailable
 
 cmdLineArgs = getArgs()
 displayServerInfo(cmdLineArgs.server)
